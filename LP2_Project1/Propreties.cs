addb56a8784b33355583adf7afdaebe32e1e99d7
Vasco Rodrigues Duarte <duarte-vasco@outlook.com>
2020-12-03 19:19:18 +0000

    Methods to reduce lines in Propreties

--- LP2_Project1/Propreties.cs
+++ LP2_Project1/Propreties.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Linq;
 using System.Globalization;
 
 namespace LP2_Project1
@@ -16,17 +15,17 @@ namespace LP2_Project1
         public string Type {get; private set;}
         public string Search {get; private set;}
         public string DiscoveryMethod {get; private set;}
-        public int[] DiscYear {get; private set;}
+        public float[] DiscYear {get; private set;}
         public float[] PlOrbper {get; private set;}
         public float[] PlRade {get; private set;}
         public float[] PlMasse {get; private set;}
         public float[] PlEqt {get; private set;}
-        public float _stTeff {get; private set;}
-        public float _stRad {get; private set;}
-        public float _stMass {get; private set;}
-        public float _stVsin {get; private set;}
-        public float _stRotp {get; private set;}
-        public float _syDist {get; private set;}
+        public float[] StTeff {get; private set;}
+        public float[] StRad {get; private set;}
+        public float[] StMass {get; private set;}
+        public float[] StVsin {get; private set;}
+        public float[] StRotp {get; private set;}
+        public float[] SyDist {get; private set;}
 
         private Propreties()
         {
@@ -42,7 +41,7 @@ namespace LP2_Project1
         }
 
         private Propreties(string file, string search, string type, string name, 
-            string discMethod, int[] discYear, float[] eqt, float[] orbPer, 
+            string discMethod, float[] discYear, float[] eqt, float[] orbPer, 
             float[] rade, float[] masse)
         {
             this.File = file;
@@ -55,6 +54,22 @@ namespace LP2_Project1
             this.PlOrbper = orbPer;
             this.PlRade = rade;
             this.PlMasse = masse;
+        }
+
+        private Propreties(string file,string search,string type,string name,
+            float[] teff,float[] rad,float[] mass,float[] vsin,float[] rotp,
+            float[] dist)
+        {
+            this.File = file;
+            this.Search = search;
+            this.Type = type;
+            this.Name = name;
+            this.StTeff = teff;
+            this.StRad = rad;
+            this.StMass = mass;
+            this.StVsin = vsin;
+            this.StRotp = rotp;
+            this.SyDist = dist;
         }
 
         public static Propreties ReadArgs(string[] args)
@@ -70,22 +85,25 @@ namespace LP2_Project1
                     search = "search";
                     propreties = SearchPlanetsOption(args, type, search);
                     break;
+
                 case "search-stars":
-                    // type = "planet";
-                    // search = "search";
-                    // propreties = SearchStarsOption(args);
-                    return new Propreties();
-                    // break;
+                    type = "star";
+                    search = "search";
+                    propreties = SearchStarsOption(args, type, search);
+                    break;
+
                 case "planet-info":
                     type = "planet";
                     search = "info";
                     propreties = InfoOption(args, type, search);
                     break;
+
                 case "star-info":
                     type = "star";
                     search = "info";
                     propreties = InfoOption(args, type, search);
                     break;
+
                 default:
                     Console.WriteLine(
                     "You will need to specify which search you want to do.");
@@ -102,7 +120,7 @@ namespace LP2_Project1
             string file = null;
             string name = null;
             string discMethod = null;
-            int[] discYear = new int [2];
+            float[] discYear = new float [2];
             float[] eqt = new float[2];
             float[] orbPer = new float [2];
             float[] rade = new float [2];
@@ -112,108 +130,27 @@ namespace LP2_Project1
             {
                 arg.ToLower();
 
-                if(arg == "--file")
-                {
-                    file = args[++index].ToLower();
-                    --index;
-                }
+                file = CondString(file, arg, "--file", index, args);
 
-                else if(arg == "--pl_name")
-                {
-                    name = args[++index].ToLower();
-                    --index;
-                }
+                name = CondString(name, arg, "--pl_name", index, args);
 
-                else if(arg == "--discmethod")
-                {
-                    discMethod = args[++index].ToLower();
-                    --index;
-                }
+                discMethod = CondString(discMethod, arg, "--discmethod", index, 
+                args);
 
-                else if((arg == "--eqt-min") || (arg == "--eqt-max"))
-                {
-                    if(arg == "--eqt-min")
-                    {
-                        eqt[0] = float.Parse(args[++index], 
-                            CultureInfo.InvariantCulture.NumberFormat); 
-                        --index;
-                    }
+                eqt = FloatMinMax(eqt, arg, "--eqt-min", "--eqt-max", 
+                index, args);
 
-                    else if(arg == "--eqt-max")
-                    {
-                        eqt[1] = float.Parse(args[++index], 
-                            CultureInfo.InvariantCulture.NumberFormat); 
-                        --index;
-                    }
-                }
+                masse = FloatMinMax(masse, arg, "--masse-min", "--masse-max", 
+                index, args);
 
-                else if((arg == "--masse-min") || (arg == "--masse-max"))
-                {
-                    if(arg == "--masse-min")
-                    {
-                        masse[0] = float.Parse(args[++index], 
-                            CultureInfo.InvariantCulture.NumberFormat); 
-                        --index;
-                    }
+                rade = FloatMinMax(rade, arg, "--rade-min", "--rade-max", 
+                index, args);
 
-                    else if(arg == "--masse-max")
-                    {
-                        masse[1] = float.Parse(args[++index], 
-                            CultureInfo.InvariantCulture.NumberFormat); 
-                        --index;
-                    }
-                }
+                orbPer = FloatMinMax(orbPer, arg, "--orbper-min", "--orbper-max"
+                , index, args);
 
-                else if((arg == "--rade-min") || (arg == "--rade-max"))
-                {
-                    if(arg == "--rade-min")
-                    {
-                        rade[0] = float.Parse(args[++index], 
-                            CultureInfo.InvariantCulture.NumberFormat); 
-                        --index;
-                    }
-
-                    else if(arg == "--rade-max")
-                    {
-                        rade[1] = float.Parse(args[++index], 
-                            CultureInfo.InvariantCulture.NumberFormat); 
-                        --index;
-                    }
-                }
-
-                else if((arg == "--orbper-min") || (arg == "--orbper-max"))
-                {
-                    if(arg == "--orbper-min")
-                    {
-                        orbPer[0] = float.Parse(args[++index], 
-                            CultureInfo.InvariantCulture.NumberFormat); 
-                        --index;
-                    }
-
-                    else if(arg == "--orbper-max")
-                    {
-                        orbPer[1] = float.Parse(args[++index], 
-                            CultureInfo.InvariantCulture.NumberFormat); 
-                        --index;
-                    }
-                }
-
-                else if((arg == "--discyear-min") || (arg == "--discyear-max"))
-                {
-                    if(arg == "--discyear-min")
-                    {
-                        discYear[0] = int.Parse(args[++index], 
-                            CultureInfo.InvariantCulture.NumberFormat); 
-                        --index;
-                    }
-
-                    else if(arg == "--discyear-max")
-                    {
-                        discYear[1] = int.Parse(args[++index], 
-                            CultureInfo.InvariantCulture.NumberFormat); 
-                        --index;
-                    }
-                }
+                discYear = FloatMinMax(discYear, arg, "--discyear-min", 
+                "--discyear-max", index, args);
 
                 index++;
             }
@@ -222,10 +159,51 @@ namespace LP2_Project1
                 discYear, eqt, orbPer, rade, masse);
         }
 
-        // private static Propreties SearchStarsOption(string[] args)
-        // {
+        private static Propreties SearchStarsOption(string[] args, string type,
+            string search)
+        {
+            int index = 0;
+            string file = null;
+            string name = null;
+            float[] teff = new float [2];
+            float[] rad = new float[2];
+            float[] mass = new float [2];
+            float[] vsin = new float [2];
+            float[] rotp = new float [2];
+            float[] dist = new float[2];
 
-        // }
+            foreach(string arg in args)
+            {
+                arg.ToLower();
+
+                file = CondString(file, arg, "--file", index, args);
+
+                name = CondString(name, arg, "--st_name", index, args);
+
+                teff = FloatMinMax(teff, arg, "--teff-min", "--teff-max", 
+                index, args);
+
+                rad = FloatMinMax(rad, arg, "--rad-min", "--rad-max", 
+                index, args);
+
+                mass = FloatMinMax(mass, arg, "--mass-min", "--mass-max", 
+                index, args);
+
+                vsin = FloatMinMax(vsin, arg, "--vsin-min", "--vsin-max", 
+                index, args);
+
+                rotp = FloatMinMax(rotp, arg, "--rotp-min", 
+                "--rotp-max", index, args);
+
+                dist = FloatMinMax(dist, arg, "--dist-min", 
+                "--dist-max", index, args);
+
+                index++;
+            }
+
+            return new Propreties(file, search, type, name, teff, 
+                rad, mass, vsin, rotp, dist);
+        }
 
         private static Propreties InfoOption(string[] args, string type, 
             string search)
@@ -249,6 +227,41 @@ namespace LP2_Project1
             }
 
             return new Propreties(file, search, type, name);
+        }
+
+        private static float[] FloatMinMax(float[] cond, string arg, 
+            string name1, string name2, int index, string[] args)
+        {
+            if((arg == name1) || (arg == name2))
+            {
+                if(arg == name1)
+                {
+                    cond[0] = int.Parse(args[++index], 
+                        CultureInfo.InvariantCulture.NumberFormat); 
+                    --index;
+                }
+
+                else if(arg == name2)
+                {
+                    cond[1] = int.Parse(args[++index], 
+                        CultureInfo.InvariantCulture.NumberFormat); 
+                    --index;
+                }
+            }
+
+            return cond;
+        }
+
+        private static string CondString(string cond, string arg, 
+            string name, int index, string[] args)
+        {
+            if(arg == name)
+            {
+                cond = args[++index].ToLower();
+                --index;
+            }
+
+            return cond;
         }
     }
 }
