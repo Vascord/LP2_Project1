6c3e1892256b42853cf43500b71a6cefe104112f
Vasco Rodrigues Duarte <duarte-vasco@outlook.com>
2020-12-07 22:11:14 +0000

    Comment Program

--- LP2_Project1/Program.cs
+++ LP2_Project1/Program.cs
@@ -8,19 +8,22 @@ namespace LP2_Project1
     class Program
     {
         /// <summary>
-        /// Static void which is the first methods that is used. It is used 
-        /// firstly to call the class ReadFile to extract the file data.
-        /// Then, it launches the the class to have the properties of the args.
-        /// After that, we call another class which will filter the data as
-        /// the user wants to and then show it to the user via the command
-        /// console.
+        /// Static void which is the first methods that is used. It creats the 
+        /// enumerables to contain the date of the planets and starts. Then, it 
+        /// calls the class Propreties to know what type of searchthe user 
+        /// wants.
+        /// Then it extracts the file data with Read File and adds to the
+        /// Enums.
+        /// After that, it do the search depending of the enums and the
+        /// propreties the user wants.
+        /// In the end, it shows in a table the results of the search.
         /// </summary>
         /// <param name="args">The arguments of the user, used as the method to 
         /// know the file and options of the reacher which the user 
         /// wants.</param>
         static void Main(string[] args)
         {
-
+            // Enumerables to keep the stars and planets for the search
             IEnumerable<Stars> stars;
             IEnumerable<Planets> planets;
 
@@ -30,18 +33,20 @@ namespace LP2_Project1
             // Extract data from the file the user wants
             ReadFile file = new ReadFile(propreties.File);
 
+            // Puts the date in the Enumerables
             planets = file.p;
             stars = file.s;
 
+            // Do the search the defined propreties and the list of stars and/or
+            // planets of the file of the user
             Search search = new Search(planets, stars, propreties);
 
+            // Actualizes the Enumerables with the search
             planets = search.planets;
             stars = search.stars;
 
+            // Shows the what the user searched
             Interface UI = new Interface(planets, stars, propreties);
-
-            
-            
         }
     }
 }
