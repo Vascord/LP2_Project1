b2f0636c2defe8bfd9bd670270495fca838a09d1
Vasco Rodrigues Duarte <duarte-vasco@outlook.com>
2020-12-08 16:50:25 +0000

    Start working on Propreties for advanced format

--- LP2_Project1/Propreties.cs
+++ LP2_Project1/Propreties.cs
@@ -259,6 +259,39 @@ namespace LP2_Project1
             this.StPls = pls;
         }
 
+        // Test forma avançada
+        private Propreties(string file, string search, string type, string name, 
+            string discMethod, string cvs, string crOrder, string drOrder, 
+            string host_name,string[] discYear, string[] eqt, string[] orbPer, 
+            string[] rade, string[] masse, string[] teff,string[] rad,
+            string[] mass, string[] vsin, string[] rotp,string[] dist, 
+            string[] age, string[] pls)
+        {
+            // Variables for propreties
+            this.File = file;
+            this.Search = search;
+            this.Type = type;
+            this.Name = name;
+            this.DiscoveryMethod = discMethod;
+            this.CVS = cvs;
+            this.CrOrder = crOrder;
+            this.DrOrder = drOrder;
+            this.HostName = host_name;
+            this.DiscYear = discYear;
+            this.PlEqt = eqt;
+            this.PlOrbper = orbPer;
+            this.PlRade = rade;
+            this.PlMasse = masse;
+            this.StTeff = teff;
+            this.StRad = rad;
+            this.StMass = mass;
+            this.StVsin = vsin;
+            this.StRotp = rotp;
+            this.SyDist = dist;
+            this.StAge = age;
+            this.StPls = pls;
+        }
+
         /// <summary>
         /// Public static method which will read the initial argument and 
         /// determinte which type of the search the user wants
@@ -278,14 +311,14 @@ namespace LP2_Project1
                 case "search-planets":
                     type = "planet";
                     search = "search";
-                    propreties = SearchPlanetsOption(args, type, search);
+                    propreties = SearchOption(args, type, search);
                     break;
 
                 //For a search of the stars
                 case "search-stars":
                     type = "star";
                     search = "search";
-                    propreties = SearchStarsOption(args, type, search);
+                    propreties = SearchOption(args, type, search);
                     break;
 
                 //For a specific search of a planet
@@ -315,6 +348,98 @@ namespace LP2_Project1
             }
 
             return propreties;
+        }
+
+        /// <summary>
+        /// Private static method to read the propreties the user wants for
+        /// search 
+        /// </summary>
+        /// <param name="args">Arguments of the user</param>
+        /// <param name="type">Planet type search</param>
+        /// <param name="search">Search type</param>
+        /// <return> The propreties for the planets search </return>
+        private static Propreties SearchOption(string[] args,string type,
+            string search)
+        {
+            // Variables of the propreties for this search
+            int index = 0;
+            string file = null, name = null, discMethod = null, cvs = null, 
+            crOrder = null, drOrder = null, hostName = null;
+            string[] discYear = new string [2], eqt = new string [2], 
+            orbPer = new string [2], rade = new string [2], 
+            masse = new string [2], teff = new string [2], 
+            rad = new string [2], mass = new string [2], vsin = new string [2], 
+            rotp = new string [2],dist = new string [2], age = new string [2], 
+            pls = new string [2];
+
+            // Reads which argument of the user to know what he wants to search
+            // If one is correct, then he will be add to the propreties, the
+            // others stay null and will not affect the search
+            foreach(string arg in args)
+            {
+                arg.ToLower();
+                
+                if(arg == "--csv") cvs = "on";
+
+                file = CondString(file, arg, "--file", index, args);
+
+                name = CondString(name, arg, "--pl_name", index, args);
+
+                discMethod = CondString(discMethod, arg, "--discmethod", index, 
+                args);
+
+                crOrder = CondString(crOrder, arg, "--cr_order", index, args);
+
+                drOrder = CondString(drOrder, arg, "--dr_order", index, args);
+
+                hostName = CondString(hostName, arg, "--hostname", 
+                    index, args);
+
+                eqt = FloatMinMax(eqt, arg, "--eqt-min", "--eqt-max", 
+                index, args);
+
+                masse = FloatMinMax(masse, arg, "--masse-min", "--masse-max", 
+                index, args);
+
+                rade = FloatMinMax(rade, arg, "--rade-min", "--rade-max", 
+                index, args);
+
+                orbPer = FloatMinMax(orbPer, arg, "--orbper-min", "--orbper-max"
+                , index, args);
+
+                discYear = FloatMinMax(discYear, arg, "--discyear-min", 
+                "--discyear-max", index, args);
+
+                teff = FloatMinMax(teff, arg, "--teff-min", "--teff-max", 
+                index, args);
+
+                rad = FloatMinMax(rad, arg, "--rad-min", "--rad-max", 
+                index, args);
+
+                mass = FloatMinMax(mass, arg, "--mass-min", "--mass-max", 
+                index, args);
+
+                vsin = FloatMinMax(vsin, arg, "--vsin-min", "--vsin-max", 
+                index, args);
+
+                rotp = FloatMinMax(rotp, arg, "--rotp-min", 
+                "--rotp-max", index, args);
+
+                dist = FloatMinMax(dist, arg, "--dist-min", 
+                "--dist-max", index, args);
+
+                age = FloatMinMax(age, arg, "--age-min", 
+                "--age-max", index, args);
+
+                pls = FloatMinMax(pls, arg, "--pls-min", 
+                "--pls-max", index, args);
+
+                index++;
+            }
+
+            return new Propreties(file, search, type, name, discMethod, cvs, 
+                crOrder, drOrder, hostName, discYear, eqt, orbPer, rade, masse,
+                teff,rad, mass, vsin, rotp, dist, age, pls);
         }
 
         /// <summary>
